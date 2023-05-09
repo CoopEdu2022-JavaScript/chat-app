16c95968c90469542cb8ab42aac3d68516fecadd
GaryZhangstu <tianrui.zhang.2101@stu.moonshotacademy.cn>
2023-05-09 13:57:33 +0800

    更新main.js

--- frontend/src/components/antdv.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import Antd from 'ant-design-vue'
-import App from './App'
-import 'ant-design-vue/dist/reset.css'
-
-const app = createApp(App)
-
-app.use(Antd).mount('#app')
