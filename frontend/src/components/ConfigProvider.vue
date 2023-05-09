9c89bdeddc89ae2faaebcbebb127fc32fd5de71a
GaryZhangstu <tianrui.zhang.2101@stu.moonshotacademy.cn>
2023-05-09 11:11:32 +0800

    配置antdv

---
+++ frontend/src/components/ConfigProvider.vue
@@ -0,0 +1,18 @@
+<template>
+  <a-config-provider :getPopupContainer="getPopupContainer">
+    <app />
+  </a-config-provider>
+</template>
+<script>
+  export default {
+    methods: {
+      getPopupContainer(el, dialogContext) {
+        if (dialogContext) {
+          return dialogContext.getDialogWrap();
+        } else {
+          return document.body;
+        }
+      },
+    },
+  };
+</script>
