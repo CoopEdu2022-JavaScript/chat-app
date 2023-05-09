7cb0d71566f48aecb19a3cad9cae5109e5ad5b62
GaryZhangstu <tianrui.zhang.2101@stu.moonshotacademy.cn>
2023-05-09 11:29:11 +0800

    修改main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import Antd from 'ant-design-vue'
+import App from './App'
+import 'ant-design-vue/dist/reset.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(Antd).mount('#app')
