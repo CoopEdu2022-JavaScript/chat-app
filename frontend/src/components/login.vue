d28a5b73cb1dd1d9af30b2a063c9a8366a8ab636
GaryZhangstu <tianrui.zhang.2101@stu.moonshotacademy.cn>
2023-05-11 13:19:20 +0800

    login

---
+++ frontend/src/components/login.vue
@@ -0,0 +1,12 @@
+<template>
+    <a-input v-model:value="value" :bordered="false" placeholder="input email" defaultValue="请输入邮箱"/>
+    <a-input-password v-model:value="value" placeholder="input password" bordered="false" defaultValue="请输入密码"/> 
+    <a-button type="primary" size="large"  @click="submit"></a-button>
+</template>
+
+<script setup>
+import { useRouter } from "vue-router"
+const router = useRouter()
+const authStore = userAuthStore()
+const token = 
+</script>
