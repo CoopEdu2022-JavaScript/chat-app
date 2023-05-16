789cdc983fad87db47aff0a36fe7dd8e6b302f06
GaryZhangstu <tianrui.zhang.2101@stu.moonshotacademy.cn>
2023-05-16 08:56:07 +0800

    header和login

--- frontend/src/components/header.vue
+++ frontend/src/components/header.vue
@@ -3,8 +3,14 @@
       v-model:value="value"
       placeholder="input search text"
       style="width: 200px"
-      @search="onSearch"
-    />
+      @search="onSearch"/>
       
-  </template>
-  
+</template>
+<script setup>
+
+
+
+</script>
+<style scoped>
+
+</style>
