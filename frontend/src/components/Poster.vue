f35d97c32b6092c8754bb9ba868b849b410c6297
GaryZhangstu <tianrui.zhang.2101@stu.moonshotacademy.cn>
2023-05-17 11:26:01 +0800

    post组件更新

---
+++ frontend/src/components/Poster.vue
@@ -0,0 +1,38 @@
+<template>
+    <hr />
+    <div @click="goDetail">
+      <img src="" alt=" ">
+      <span >{{ nickname }}</span>
+      <span>&nbsp;</span>
+      <span >{{ date }}</span>
+      <p >{{ text }}</p>
+      <div class="image">imgs: 
+        <img src="" alt=" ">
+        <img src="" alt=" ">
+        <img src="" alt=" ">
+        <img src="" alt=" ">
+      </div>
+      <span >{{ comment }}</span>
+      <span>&nbsp;</span>
+      <span >{{ like }}</span>
+    </div>
+</template>
+  
+<script setup>
+  import http from '../api/http';
+  
+  let nickname = 'nickname: '
+  let date = 'date: '
+  let text = 'text content: '
+  let comment = 'comment: '
+  let like = 'like: '
+  
+  const router = useRouter()
+  const goDetail = () => {
+    router.push('/feed/detail')
+  }
+  
+</script>
+  
+<style scoped>
+</style>
---
+++ frontend/src/components/poster-test.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="clearfix">
+      <a-upload
+        v-model:file-list="fileList"
+        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+        list-type="picture-card"
+        @preview="handlePreview"
+      >
+        <div v-if="fileList.length < 8">
+          <plus-outlined />
+          <div style="margin-top: 8px">Upload</div>
+        </div>
+      </a-upload>
+      <a-modal :open="previewVisible" :title="previewTitle" :footer="null" @cancel="handleCancel">
+        <img alt="example" style="width: 100%" :src="previewImage" />
+      </a-modal>
+    </div>
+  </template>
+  <script>
+  import { ref } from 'vue';
+  function getBase64(file) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = error => reject(error);
+    });
+  }
+  const previewVisible = ref(false);
+  const previewImage = ref('');
+  const previewTitle = ref('');
+  const fileList = ref([{
+    uid: '-1',
+    name: 'image.png',
+    status: 'done',
+    url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+  }, {
+    uid: '-2',
+    name: 'image.png',
+    status: 'done',
+    url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+  }, {
+    uid: '-3',
+    name: 'image.png',
+    status: 'done',
+    url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+  }, {
+    uid: '-4',
+    name: 'image.png',
+    status: 'done',
+    url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+  }, {
+    uid: '-xxx',
+    percent: 50,
+    name: 'image.png',
+    status: 'uploading',
+    url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+  }, {
+    uid: '-5',
+    name: 'image.png',
+    status: 'error',
+  }]);
+  const handleCancel = () => {
+    previewVisible.value = false;
+    previewTitle.value = '';
+  };
+  const handlePreview = async file => {
+    if (!file.url && !file.preview) {
+      file.preview = await getBase64(file.originFileObj);
+    }
+    previewImage.value = file.url || file.preview;
+    previewVisible.value = true;
+    previewTitle.value = file.name || file.url.substring(file.url.lastIndexOf('/') + 1);
+  };
+  </script>
+  <style scoped>
+  /* you can make up upload button and sample style by using stylesheets */
+  .ant-upload-select-picture-card i {
+    font-size: 32px;
+    color: #999;
+  }
+  
+  .ant-upload-select-picture-card .ant-upload-text {
+    margin-top: 8px;
+    color: #666;
+  }
+  </style>
