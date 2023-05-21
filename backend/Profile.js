8241a62fe4e45bc7e1d15e6615a17acb1fb8e9e6
GaryZhangstu <tianrui.zhang.2101@stu.moonshotacademy.cn>
2023-05-21 22:49:31 +0800

    profile后端

--- backend/Profile.js
+++ backend/Profile.js
@@ -1,7 +1,22 @@
 const express = require('express')
-
+const app = express()
 const router = express.Router()
+const cors = require('cors')
+const port = 3000
+const path = require('path')
+const mysql = require('mysql')
+const crypto = require('crypto')
 
 router.use(express.json())
-router.use(express.urlencoded({ extended: true }));
+router.use(express.urlencoded({ extended: true }))
 
+router.get('/like', (req, res) => {
+  let { like } = req.body;  // true false
+
+  res.send(is_success)
+})
+router.post('/login', (req, res) => {
+  let { uid,name} = req.body
+    res.send(uid == 'admin' && pwd == 'admin')
+  })
+module.exports = router
--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,16 @@ const express = require('express')
 const app = express()
 const router = require('./router')
 const cors = require('cors')
+const port = 3000
+app.use(express.json())
 
 const corsOptions = {
   origin: 'http://localhost:5173'
-};
-app.use(cors(corsOptions));
+}
+app.use(cors(corsOptions))
 
 router(app)
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server is running on http://localhost:3000')
 })
