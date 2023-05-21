12017dfd07484a14be0a3e19e45db38fec928860
GaryZhangstu <tianrui.zhang.2101@stu.moonshotacademy.cn>
2023-05-21 23:05:25 +0800

    profile修改

--- backend/Profile.js
+++ backend/Profile.js
@@ -6,17 +6,30 @@ const port = 3000
 const path = require('path')
 const mysql = require('mysql')
 const crypto = require('crypto')
+const { MongoClient, ServerApiVersion } = require('mongodb')
+const uri = 'mongodb+srv://<username>:<password>@cluster0.urecvjg.mongodb.net/?retryWrites=true&w=majority'
 
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+})
+async function run () {
+  try {
+    await client.connect()
+
+    await client.db('admin').command({ ping: 1 })
+    console.log('Pinged your deployment. You successfully connected to MongoDB!')
+  } finally {
+    await client.close()
+  }
+}
+run().catch(console.dir)
 router.use(express.json())
 router.use(express.urlencoded({ extended: true }))
 
-router.get('/like', (req, res) => {
-  let { like } = req.body;  // true false
-
-  res.send(is_success)
+router.get('/profile', (req, res) => {
+  const name = req.body
 })
-router.post('/login', (req, res) => {
-  let { uid,name} = req.body
-    res.send(uid == 'admin' && pwd == 'admin')
-  })
-module.exports = router
