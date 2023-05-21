c47b08f50dbc4aba91b08a23026e7c96474d68a9
17354 <cheng.he.stu@2101.moonshotacademy.cn>
2023-05-21 19:20:29 +0800

    login bug fixed

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,7 +2,6 @@
     <router-view />
     <div>token={{ token }}</div>
 </template>
-  
 <script setup>
 import { storeToRefs } from 'pinia'
 const userStore = useUserStore()
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,23 +1,9 @@
 import { createApp } from 'vue'
-import { createStore } from 'vuex' // 导入 createStore
 import Antd from 'ant-design-vue'
 import App from './App.vue'
 import router from './router'
 import 'ant-design-vue/dist/reset.css'
 
-const store = createStore({ // 创建 store
-    state: {
-        count: 0
-    },
-    mutations: {
-        increment(state) {
-            state.count++
-        }
-    }
-})
-
 const app = createApp(App)
-app.use(router)
-app.use(store)
-app.use(Antd)
-app.mount('#app')
+
+app.use(Antd).use(router).mount('#app')
