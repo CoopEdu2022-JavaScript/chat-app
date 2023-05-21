7f44ecc05d05a15065f47c9a1f5bea1c00974267
17354 <cheng.he.stu@2101.moonshotacademy.cn>
2023-05-21 19:54:24 +0800

    front

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,6 +4,7 @@
 </template>
 <script setup>
 import { storeToRefs } from 'pinia'
+import { useUserStore } from '../store/user.js'
 const userStore = useUserStore()
 let { token } = storeToRefs(userStore)
 </script>
