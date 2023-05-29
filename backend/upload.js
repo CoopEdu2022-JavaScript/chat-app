d03e2650361aa40fd9eb6d9a9b1b5ce337a95490
GaryZhangstu <tianrui.zhang.2101@stu.moonshotacademy.cn>
2023-05-29 21:19:59 +0800

    likes like unlike upload图片 post功能修改

--- backend/upload.js
+++ backend/upload.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const multer = require('multer')
 const mysql = require('mysql2/promise')
-
+const router = express.Router()
 const app = express()
 
 // 配置 multer 中间件
@@ -24,28 +24,51 @@ const pool = mysql.createPool({
 })
 
 // 处理上传请求
-app.post('/upload', upload.array('images', 9), async function (req, res, next) {
-    const files = req.files
-    if (!files || files.length === 0) {
-      const error = new Error('Please upload at least one file')
-      error.status = 400
-      return next(error)
-    }
-    try {
-      // 将文件信息存入数据库
-      const conn = await pool.getConnection()
-      const promises = files.map(file => {
-        return conn.query('INSERT INTO images (filename, path) VALUES (?, ?)', [file.filename, file.path])
-      })
-      await Promise.all(promises)
-      conn.release()
-      res.send('Files uploaded successfully')
-    } catch (err) {
-      console.error('Error uploading files:', err)
-      res.status(500).json({ error: 'Internal server error' })
-    }
-  })
+router.post('/upload', upload.array('images', 9), async function (req, res, next) {
+	const { post_id } = req.query
+	const files = req.files
+	if (!files || files.length === 0) {
+		const error = new Error('Please upload at least one file')
+		error.status = 400
+		return next(error)
+	}
+	try {
+		// 将文件信息存入数据库
+		const conn = await pool.getConnection()
+		const promises = files.map(file => {
+			return conn.query('INSERT INTO images (filename, path) VALUES (?, ?) WHERE post_id = ?' [file.filename, file.path, post_id])
+		});
+		await Promise.all(promises)
+		conn.release()
+		res.send('Files uploaded successfully')
+	} catch (err) {
+		console.error('Error uploading files:', err)
+		res.status(500).json({ error: 'Internal server error' })
+	}
+})
 
-app.listen(3000, function () {
+router.get('/images', async function (req, res) {
+	const { post_id } = req.query
+	try {
+		// Fetch image data from database
+		const conn = await pool.getConnection()
+		const [rows] = await conn.query('SELECT * FROM images')
+		conn.release()
+		// Map rows to image objects
+		const images = rows.map(row => {
+			return {
+				filename: row.filename,
+				path: row.path
+			}
+		})
+		res.json(images)
+	} catch (err) {
+		console.error('Error fetching images:', err)
+		res.status(500).json({ error: 'Internal server error' })
+	}
+})
+
+
+router.listen(3000, function () {
   console.log('Server started on port 3000')
 })
