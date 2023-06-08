255f6e7c8107cc154369e36a36a9dd9ebbdc872b
GaryZhangstu <tianrui.zhang.2101@stu.moonshotacademy.cn>
2023-06-08 14:30:42 +0800

    修改login

---
+++ backend/test5.js
@@ -0,0 +1,11 @@
+const pool = require('./db')
+
+async function getUsers() {
+    
+    const sql = `SELECT likes FROM post WHERE post_id = ?;SELECT * FROM like_post WHERE post_id = ?, user_id = ?`
+    const values = [id]
+    const [rows, fields] = await pool.execute(sql,values)
+    console.log(rows[0])
+    return rows
+  }
+getUsers()
