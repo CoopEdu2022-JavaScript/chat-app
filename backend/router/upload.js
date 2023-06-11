8666e84cfab12f7a1a9f71c422c0fa471b1e925d
GaryZhangstu <tianrui.zhang.2101@stu.moonshotacademy.cn>
2023-06-11 16:31:55 +0800

    支持上传头像（icon）

--- backend/router/upload.js
+++ backend/router/upload.js
@@ -30,19 +30,35 @@ router.post('/:id/upload', upload.single('image'), async (req, res) => {
     res.status(500).json({ error: 'Internal server error' })
   }
 })
-router.get('/:id', async (req, res) => {
+// router.get('/:id', async (req, res) => {
+//   try {
+//     const id  = req.params.id
+//     const sql = 'SELECT * FROM post WHERE post_id = ?'
+//     const values = [id]
+//     const [rows] = await db.query(sql, values)
+//     res.json(rows[0])
+//   } catch (err) {
+//     console.error('Error fetching post:', err)
+//     res.status(500).json({ err })
+//   }
+// })
+router.post('/icon', upload.single('image'), async (req, res) => {
+  
+  const user_id = getPayload(req).user_id
+  const { originalname, mimetype, filename, path, size } = req.file
+  const sql = 'INSERT INTO images_icon (name, type, filename, path, size,uid) VALUES (?,?, ?, ?, ?, ?)'
+  const values = [originalname, mimetype, filename, path, size,user_id]
   try {
-    const id  = req.params.id
-    const sql = 'SELECT * FROM post WHERE post_id = ?'
-    const values = [id]
-    const [rows] = await db.query(sql, values)
-    res.json(rows[0])
+    const conn = await db.getConnection()
+    const [rows] = await conn.query(sql, values)
+    conn.release()
+    console.log('File uploaded successfully')
+    res.send('File uploaded successfully')
   } catch (err) {
-    console.error('Error fetching post:', err)
-    res.status(500).json({ err })
+    console.error('Error uploading file:', err)
+    res.status(500).json({ error: 'Internal server error' })
   }
 })
-
 router.get('/:id', async function (req, res, next) {
   const { id } = req.params
   const user_id = getPayload(req).user_id
@@ -66,5 +82,28 @@ router.get('/:id', async function (req, res, next) {
     next(error)
   }
 })
+router.get('/icon', async function (req, res, next) {
+  const user_id = getPayload(req).user_id
+  try {
+    const [rows] = await db.query('SELECT * FROM images_icon WHERE uid = ?', [user_id])
+    if (rows.length === 0) {
+      const error = new Error('Image not found')
+      error.status = 404
+      return next(error)
+    }
+    const { filename, mimetype, size } = rows[0]
+    const filePath = path.join(__dirname, '..', 'uploads', filename)
+    const fileStream = fs.createReadStream(filePath)
+    res.setHeader('Content-Type', mimetype)
+    res.setHeader('Content-Length', size)
+    fileStream.pipe(res)
+  } catch (err) {
+    console.error('Error retrieving image:', err)
+    const error = new Error('Internal server error')
+    error.status = 500
+    next(error)
+  }
+})  
+
 
 module.exports = router
