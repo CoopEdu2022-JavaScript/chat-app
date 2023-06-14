9d5a5395224e915dd78afdb5d766da0d5f86b65d
17354 <cheng.he.stu@2101.moonshotacademy.cn>
2023-06-14 17:34:01 +0800

    Backend some bug fixes

--- backend/router/post.js
+++ backend/router/post.js
@@ -3,23 +3,23 @@ const express = require('express')
 const router = express.Router()
 const db = require('../db')
 const { getPayload } = require('../jwt_config')
-const {v5 : uuidv5} = require("uuid")
+const { v5: uuidv5 } = require("uuid")
 router.use(express.json())
 router.use(express.urlencoded({ extended: true }))
 
 router.post('/newpost', async (req, res) => {
   try {
-    
+
     const { user_id } = getPayload(req)
-    const { title, content} = req.body
+    const { title, content } = req.body
     const namespace = '8e884ace-bee4-11e4-8dfc-aa07a5b093db'
     const time = new Date().getTime().toString()
     const uuid = uuidv5(`${title}${content}${user_id}${time}`, namespace)
-    const sql = `INSERT INTO post (title, content,date, uid, likes,coments_id,post_id,images) VALUES (? ,?, NOW(),?, 0,0,?,0)`
-    const values = [title, content, user_id, uuid]
-    const [rows]= await db.query(sql,values)
-    
-    res.json({state:true , post_id:uuid})
+    const sql = `INSERT INTO post (title, content,date, uid, likes,coments_id,post_id,images) VALUES ('${title}', '${content}', NOW(),${user_id}, 0,0,'${uuid}',9099)`
+
+    const [rows] = await db.query(sql)
+
+    res.json({ state: true, post_id: uuid })
   } catch (err) {
     console.error('Error creating post:', err)
     res.status(500).json({ err })
@@ -29,7 +29,7 @@ router.post('/newpost', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const { user_id } = getPayload(req)
-    const id  = req.params.id
+    const id = req.params.id
     const sql = 'SELECT * FROM post WHERE post_id = ?'
     const values = [id]
     const [rows] = await db.query(sql, values)
@@ -42,12 +42,11 @@ router.get('/:id', async (req, res) => {
 router.put('/:id/fix', async (req, res) => {
   try {
     const { user_id } = getPayload(req)
-    const id  = req.params.id
-    const { title, content} = req.body
-    const sql = `UPDATE post SET title = ?, content = ? ,date = NOW() WHERE post_id = ? AND uid = ?`
-    const values = [title, content, id, user_id]
-    const [rows] = await db.query(sql, values)
-    res.json({state:true})
+    const id = req.params.id
+    const { title, content } = req.body
+    const sql = `UPDATE post SET title = '${title}', content = '${content}' ,date = NOW() WHERE post_id = ${id} AND uid = ${user_id}`
+    const [rows] = await db.query(sql)
+    res.json({ state: true })
   } catch (err) {
     console.error('Error fetching post:', err)
     res.status(500).json({ err })
@@ -56,18 +55,17 @@ router.put('/:id/fix', async (req, res) => {
 router.delete('/:id/delete', async (req, res) => {
   try {
     const { user_id } = getPayload(req)
-    const id  = req.params.id
-    const sql = `DELETE FROM post WHERE post_id =?;DELETE FROM like_post WHERE post_id = ?;DELETE FROM comment WHERE post_id = ? ;DELETE FROM conment WHERE post_id = ?;Delete FROM images_post WHERE post_id = ?`
-    const values = [id, id, id, id, id]
+    const id = req.params.id
+    const sql = `DELETE FROM post WHERE post_id = ${id};DELETE FROM like_post WHERE post_id = ${id};DELETE FROM comment WHERE post_id = ${id} ;DELETE FROM conment WHERE post_id = ${id};Delete FROM images_post WHERE post_id = ${id}`
     const [rows] = await db.query(sql)
-    res.json({state:true})
+    res.json({ state: true })
   } catch (err) {
     console.error('Error fetching post:', err)
     res.status(500).json({ err })
   }
 })
 
-router.put('/:id/likes', async(req, res) => {
+router.post('/:id/likes', async (req, res) => {
   // const { id } = req.params
   // db.query('UPDATE post SET likes = likes + 1 WHERE id = ?', [id], (err, data) => {
   //   if (err) res.status(500).json({ err })
@@ -75,64 +73,63 @@ router.put('/:id/likes', async(req, res) => {
   // })
   try {
     const { user_id } = getPayload(req)
-    const id  = req.params.id
+    const id = req.params.id
     const sql = `INSERT INTO like_post (post_id, uid, time) VALUES (?, ?, NOW());
     UPDATE post SET likes = likes + 1 WHERE post_id = ?;`
     const values = [id, user_id, id]
     const [rows] = await db.query(sql, values)
-    
-    res.json({state:true})
-  } catch(err){
+
+    res.send({ state: true })
+  } catch (err) {
     console.error('not success', err)
-    res.status(500).json({err})
+    res.status(500).json({ err })
   }
-  
+
 })
 
-router.put('/:id/unlike', async(req, res) => {
+router.post('/:id/unlike', async (req, res) => {
   // const { id } = req.params
   // db.query('UPDATE post SET likes = likes - 1 WHERE id = ?', [id], (err, data) => {
   //   if (err) res.status(500).json({ err })
   //   else res.send(true)
   // })
   try {
-    const id  = req.params.id
+    const id = req.params.id
     const { user_id } = getPayload(req)
-    const sql = `DELETE FROM like_post WHERE post_id = ? AND uid =?;
-    UPDATE post SET likes = likes - 1 WHERE post_id = ?;`
-    const values = [id, user_id, id]
-  
-    const [rows] = await db.query(sql, values)
+    const sql = `DELETE FROM like_post WHERE post_id = ? AND uid = ?;
+    UPDATE post SET likes = likes - 1 WHERE post_id = ?`
+    const values = [id,user_id,id]
+    const [rows] = await db.query(sql,values)
     res.send(true)
-  } catch(err){
+  } catch (err) {
     console.error('not success', err)
-    res.status(500).json({err})
+    res.status(500).json({ err })
   }
 
 })
 
-router.get('/:id/hlike', async(req, res) => {
+router.get('/:id/hlike', async (req, res) => {
   // const { id } = req.params
   // db.query('SELECT likes FROM post WHERE id = ?', [id], (err, data) => {
   //   if (err) res.status(500).json({ err })
   //   else res.send(data)
   // })
-  
+
   try {
     const { user_id } = getPayload(req)
-    const id  = req.params.id
+    const id = req.params.id
     const sql = 'SELECT likes FROM post WHERE post_id = ?;SELECT * FROM like_post WHERE post_id = ? AND uid = ?'
     const values = [id, id, user_id]
     const [rows] = await db.query(sql, values)
     let isLiked = false;
     if (!rows[0][1] == null) isLiked = true;
     res.json({ likes: rows[0][0].likes, isLiked })
-    
+
   } catch (err) {
     console.error('Error fetching likes:', err)
     res.status(500).json({ err })
   }
-})                                                      
+})
 
 router.get('/users/getallpost', async (req, res) => {
   try {
@@ -150,7 +147,7 @@ router.get('/users/getallpost', async (req, res) => {
 router.get('/usersname/:id', async (req, res) => {
   try {
     const { user_id } = getPayload(req)
-    const id  = req.params.id
+    const id = req.params.id
     const sql = 'SELECT usernames FROM users WHERE uid = ? LIMIT 1'
     const values = [id]
     const [rows] = await db.query(sql, values)
@@ -161,59 +158,43 @@ router.get('/usersname/:id', async (req, res) => {
   }
 })
 
-router.get(':id/users', async (req, res) => {
+router.get('/:id/users', async (req, res) => {
   try {
 
-    const { user_id } = getPayload(req)
-    let all = []
-    const id  = req.params.id
+    const { user_id } = getPayload(req) 
+    const id = req.params.id
     const sql = 'SELECT uid FROM post WHERE post_id = ?'
     const values = [id]
-
     const [rows] = await db.query(sql, values)
     const sql2 = 'SELECT usernames FROM users WHERE uid = ? LIMIT 1'
     const values2 = [rows[0].uid]
     const [rows2] = await db.query(sql2, values2)
-    const sql3 = 'SELECT * FROM images_post WHERE post_id = ?'
-    const values3 = [id]
-    const [rows3] = await db.query(sql3, values3)
-    const sql4 = 'SELECT * FROM conment WHERE post_id = ?'
-    const values4 = [id]
-    const [rows4] = await db.query(sql4, values4)
-    all.push.apply(all, rows, rows2, rows3, rows4)
-    res.json(all)
-    
+    res.send(rows2[0])
+
   } catch (err) {
-    console.error('Error fetching post:', err)
+    console.log(err)
+    // console.error('Error fetching post:', err)
     res.status(500).json({ err })
   }
 })
 
-router.get('/:id/detail', async (req, res) => {
-  try {
-    const { user_id } = getPayload(req)
-    const id  = req.params.id
-    const sql = 'SELECT * FROM post WHERE post_id = ?'
-    const values = [id]
-    const [rows] = await db.query(sql, values)
-    const sql2 = 'SELECT usernames FROM users WHERE uid = ? LIMIT 1'
-    const values2 = [rows[0].uid]
-    const [rows2] = await db.query(sql2, values2)
-    const sql3 = 'SELECT * FROM images_post WHERE post_id = ?'
-    const values3 = [id]
-    const [rows3] = await db.query(sql3, values3)
-    const sql4 = 'SELECT * FROM conment WHERE post_id = ?'
-    const values4 = [id]
-    const [rows4] = await db.query(sql4, values4)
-    res.json({ ...rows[0], usernames: rows2[0].usernames, images: rows3, conment: rows4 })
+// router.get('/:id/detail', async (req, res) => {
+//   try {
+//     const { user_id } = getPayload(req)
+//     const id  = req.params.id
+//     const sql = 'SELECT * FROM post WHERE post_id = ?'
+//     const values = [id]
+//     const [rows] = await db.query(sql, values)
+//     const sql2 = 'SELECT usernames FROM users WHERE uid = ? LIMIT 1'
+//     const values2 = [rows[0].uid]
+//     const [rows2] = await db.query(sql2, values2)
+//     const sql3 = 'SELECT * FROM images_post WHERE post_id = ?'
+//     const values3 = [id]
+//     const [rows3] = await db.query(sql3, values3)
+//     const sql4 = 'SELECT * FROM like_post WHERE post_id = ?'
+//     const values4 = [id]
 
+//     res.json({ ...rows[0], usernames: rows2[0].usernames, images: rows3 })
 
-    
-  } catch (err) {
-    console.error('Error fetching post:', err)
-    res.status(500).json({ err })
-  }
-})
-    
 module.exports = router
 
